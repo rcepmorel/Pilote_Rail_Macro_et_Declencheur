ccf0523dfdf2973e40a4d5451ff1db748e12fe70
rcepmorel <47545037+rcepmorel@users.noreply.github.com>
2019-03-03 03:33:33 -0500

    Add files via upload

---
+++ Pilote_Rail_Macro_et_Declencheur_00/actions.h
@@ -0,0 +1,302 @@
+//*************   Avancement - Chariot en fin de course ***************
+void test_FinAVNC()                    
+{
+  if (digitalRead(PinIntFinAVNC) == HIGH) {
+    digitalWrite(pinEnable8825, LOW);  // Active la sortie moteur
+  } else {
+    digitalWrite(pinEnable8825, HIGH); // Désactive la sortie moteur
+    Serial.print( "RENDU EN FIN DE COURSE - " );
+    intNbreDePasEntreArretsB = 0;
+    intNbreDeBoucleB         = 0;
+    intDelaiInterBoucleB     = 0; 
+  }
+}
+
+//*************  Recul - Chariot en fin de course *********************
+void test_FinRCL()                     
+{
+  if (digitalRead(PinIntFinRCL) == HIGH) {
+    digitalWrite(pinEnable8825, LOW);   // Active la sortie moteur
+  } else {
+    digitalWrite(pinEnable8825, HIGH);  // Désactive la sortie moteur
+    Serial.print( "RENDU EN FIN DE COURSE - " );
+    intNbreDePasEntreArretsB = 0;
+    intNbreDeBoucleB         = 0;
+    intDelaiInterBoucleB     = 0;
+  }
+}
+
+
+//*****************   Déclenche l'appareil photo   ********************
+void declencheApp(){                  
+  Serial.println( "CLICK " );
+  digitalWrite( PIN_ACTIVE_RELAIS, HIGH);           
+  delay(100);
+  digitalWrite( PIN_ACTIVE_RELAIS, LOW);
+  delay(intDelaiApresDeclenchement);  // Délai après déclenchement
+  
+  } 
+
+//*****************   Exécute le déplacement vers l'avant   ********************
+void execDeplacementAvance(){ 
+  // intCompteurdePasRef -> sert pour accumuler le nombre de pas 
+  // entre la référence A et la référence B
+  // Si intCompteurdePasRef == 0 , on établi la direction
+  // d'incrémentation du calcul de pas 
+                     
+  if ((intCompteurdePasRef == 0)&&( strDirectionRef==""))
+    {
+      strDirectionRef = "Avance";
+    }
+  for ( compteur = 0; compteur < intNbreDeBoucleB; compteur++) {
+    // Avance de X pas
+    for ( i = 0; i < intNbreDePasEntreArretsB; i++) {
+      test_FinAVNC();
+      digitalWrite( pinStep8825, HIGH );
+      delayMicroseconds(timePulseStep);
+      digitalWrite( pinStep8825, LOW );
+      delayMicroseconds(timePulseStep);
+      if (strDirectionRef == "Avance")
+        {
+          intCompteurdePasRef++;
+        }
+        else
+        {
+          intCompteurdePasRef --;
+        }
+    }
+    delay(intDelaiInterBoucleB); 
+  }
+}
+
+//*****************   Exécute le déplacement vers l'arrière   ********************
+void execDeplacementRecule(){ 
+    if ((intCompteurdePasRef == 0)&&( strDirectionRef==""))
+    {
+      strDirectionRef = "Recule";
+    }
+  for ( compteur = 0; compteur < intNbreDeBoucleB; compteur++) {
+    // Avance de X pas
+    for ( i = 0; i < intNbreDePasEntreArretsB; i++) {
+      test_FinRCL();
+      digitalWrite( pinStep8825, HIGH );
+      delayMicroseconds(timePulseStep);
+      digitalWrite( pinStep8825, LOW );
+      delayMicroseconds(timePulseStep);
+      if (strDirectionRef == "Recule")
+        {
+          intCompteurdePasRef++;
+        }
+        else 
+        {
+          intCompteurdePasRef --;
+        }      
+    } 
+    delay(intDelaiInterBoucleB);      // attente
+  }
+}
+  
+//*****************   Avance très rapide   *****************************
+void go_AvanceTresRapide()            
+{
+  digitalWrite( pinDir8825,    HIGH); // Direction avant
+  digitalWrite( pinStep8825,   LOW);  // Initialisation de la broche step
+  digitalWrite( pinEnable8825, LOW ); // Active la sortie moteur
+  intNbreDePasEntreArretsB   = 200;   // 200 * 0.02mm -> 4mm
+  intNbreDeBoucleB           = 10;    // 10*4mm -> 40 mm -> 4cm
+  intDelaiInterBoucleB       = 25; 
+  execDeplacementAvance();
+  Serial.println( "Avance très rapide terminé" );
+}
+
+//*****************   Avance rapide   *****************************
+void go_AvanceRapide()               
+{
+  digitalWrite( pinDir8825,    HIGH); // Direction avant
+  digitalWrite( pinStep8825,   LOW);  // Initialisation de la broche step
+  digitalWrite( pinEnable8825, LOW ); // Active la sortie moteur
+  intNbreDePasEntreArretsB   = 200;   // 200 * 0.02mm -> 4mm
+  intNbreDeBoucleB           = 3;     // 3*4mm -> 12 mm -> 1.2cm
+  intDelaiInterBoucleB       = 25; 
+  execDeplacementAvance();
+  Serial.println( "Avance rapide terminé" );
+}
+
+//*****************   Avance lente   *****************************
+void go_AvanceLente()                 // Avance lente
+{
+  digitalWrite( pinDir8825,    HIGH); // Direction avant
+  digitalWrite( pinStep8825,   LOW);  // Initialisation de la broche step
+  digitalWrite( pinEnable8825, LOW ); // Active la sortie moteur
+  intNbreDePasEntreArretsB   = 25;    // 25 * 0.02mm -> 0.5mm
+  intNbreDeBoucleB           = 1;     
+  intDelaiInterBoucleB       = 0;
+  execDeplacementAvance();
+  Serial.println( "Avance lente terminé" );
+}
+
+//******************   Arrêt du moteur  ********************************
+void stop_Stop()  //Arrêt du moteur
+{
+  digitalWrite( pinEnable8825, HIGH ); // Désactive la sortie moteur
+  Serial.println( "STOP" );
+}
+
+//*******************  Recule lente  ************************************
+void go_ReculeLente()                  
+{
+  digitalWrite( pinDir8825,    LOW);   // Direction recule
+  digitalWrite( pinStep8825,   LOW);   // Initialisation de la broche step
+  digitalWrite( pinEnable8825, LOW );  // Active la sortie moteur
+  intNbreDePasEntreArretsB   = 25;     // 25 * 0.02mm -> 0.5mm
+  intNbreDeBoucleB           = 1;      
+  intDelaiInterBoucleB       = 0;
+  execDeplacementRecule();
+  Serial.println( "Recule terminé" );
+}
+
+//*******************  Recule rapide  ***********************************
+void go_ReculeRapide()                
+{
+  digitalWrite( pinDir8825,    LOW);  // Direction recule
+  digitalWrite( pinStep8825,   LOW);  // Initialisation de la broche step
+  digitalWrite( pinEnable8825, LOW ); // Active la sortie moteur
+  intNbreDePasEntreArretsB   = 200;   // 200 * 0.02mm -> 4mm
+  intNbreDeBoucleB           = 3;     // 3*4mm -> 12 mm -> 1.2cm
+  intDelaiInterBoucleB       = 25;
+  execDeplacementRecule();
+  Serial.println( "Recule rapide termine" );
+}
+
+//*******************  Recule très rapide  *****************************
+void go_ReculeTresRapide()        
+{
+  digitalWrite( pinDir8825,    LOW);  // Direction recule
+  digitalWrite( pinStep8825,   LOW);  // Initialisation de la broche step
+  digitalWrite( pinEnable8825, LOW ); // Active la sortie moteur
+  intNbreDePasEntreArretsB   = 200;   // 200 * 0.02mm -> 4mm
+  intNbreDeBoucleB           = 10;    // 10*4mm -> 40 mm -> 4cm
+  intDelaiInterBoucleB       = 25;    
+  execDeplacementRecule();
+  Serial.println( "Recule très rapide terminé" );
+}
+
+//-------------------- AVANCE CAPTURE ------------------------------- 
+void go_AvanceCapture()                        // Avance et capture la photo
+{
+  digitalWrite( pinDir8825,  HIGH);            // Direction avant
+  digitalWrite( pinStep8825, LOW);             // Initialisation de la broche step
+  intPasAccompli = 0;
+  for ( compteur = 0; compteur < intNbreDeBoucle; compteur++) {
+    declencheApp();                            // Déclenche l'appareil photo 
+    digitalWrite( pinEnable8825, LOW );        // Active la sortie moteur
+    // Avance de X pas
+    for ( i = 0; i < intNbreDePasEntreArrets; i++) {
+      test_FinAVNC();                          // Vérifie si arrivé à la fin du rail
+      if (digitalRead(PinIntFinAVNC) == LOW) {
+        i = intNbreDePasEntreArrets;
+        compteur  = intNbreDeBoucle;
+      }
+      intPasAccompli++;
+      digitalWrite( pinStep8825, HIGH );
+      delayMicroseconds(timePulseStep);
+      digitalWrite( pinStep8825, LOW );
+      delayMicroseconds(timePulseStep);
+    }
+          if (intDesacMotPausBcl == 1)         // Désactive le moteur
+        {
+          digitalWrite( pinEnable8825, HIGH ); // Désactive la sortie moteur
+        }
+    //******** Attention : éviter serial.println ici, cela fait planter l'application android
+    do_Web();                                  // Attention : ajoute un délai
+    if (boExecStop) {
+       boExecStop = false;
+       compteur   = intNbreDeBoucle;
+       stop_Stop();  
+    }
+    delay(intDelaiAvantDeclenchement);         // attente stabilité puis pose
+   Serial.println( String(compteur+1) + "/" + String(intNbreDeBoucle)+ " " );   
+  }
+  declencheApp();                              // déclenche l'appareil photo 
+  Serial.println( "Avance et capture terminé" );
+  if (intRetourAuto == 1)                      // retour au point de départ
+  {
+    digitalWrite( pinDir8825,    LOW );        // inverse la direction
+    digitalWrite( pinEnable8825, LOW );        // Active la sortie moteur
+    for ( i = 0; i < intPasAccompli; i++) {
+      digitalWrite( pinStep8825, HIGH );
+      delayMicroseconds(timePulseStep);
+      digitalWrite( pinStep8825, LOW );
+      delayMicroseconds(timePulseStep);
+      test_FinRCL();   // ne devrait pas arriver sauf par une erreur de manipulation
+      if (digitalRead(PinIntFinRCL) == LOW) {
+        i = intPasAccompli;
+      }
+    }
+  }
+  if (intStopFinBoucle == 1)                   // STOP à la fin
+  {
+    digitalWrite( pinEnable8825, HIGH );       // Désactive la sortie moteur     
+  }
+}
+//----------------- RECULE CAPTURE --------------------------------------- 
+void go_ReculeCapture()                        // Recule et capture la photo
+{
+  digitalWrite( pinDir8825,      LOW );        // Direction recule
+  digitalWrite( pinStep8825,     LOW ); 
+  
+  intPasAccompli=0;
+  for ( compteur = 0; compteur < intNbreDeBoucle; compteur++) {
+    declencheApp();                            // déclenche l'appareil photo 
+    digitalWrite( pinEnable8825, LOW );        // Active la sortie moteur    
+    // Avance de X pas
+    for ( i = 0; i < intNbreDePasEntreArrets; i++) {
+      test_FinRCL();                           // Vérifie si arrivé à la fin du rail
+       if (digitalRead(PinIntFinRCL) == LOW) {
+        i = intNbreDePasEntreArrets;
+        compteur  = intNbreDeBoucle;
+      }
+      intPasAccompli++; // Accumule le nombre de pas accompli
+      digitalWrite( pinStep8825, HIGH );
+      delayMicroseconds(timePulseStep);
+      digitalWrite( pinStep8825, LOW );
+      delayMicroseconds(timePulseStep);
+    }
+      if (intDesacMotPausBcl== 1)              // Désactive le moteur
+        {
+          digitalWrite( pinEnable8825, HIGH ); // Désactive le moteur     
+        }
+    //******** Attention: éviter serial.println ici, cela fait planter l'application android
+    do_Web();                                 // Attention : ajoute un délai
+    if (boExecStop) {
+       boExecStop = false;
+       compteur = intNbreDeBoucle;
+       stop_Stop();  
+    }
+    Serial.println( String(compteur+1) + "/" + String(intNbreDeBoucle)+ " " );
+    delay(intDelaiAvantDeclenchement);         // attente stabilité puis pose
+  } // fin de for ( compteur = 0; compteur < intNbreDeBoucle; compteur++)
+  
+  declencheApp();                              // déclenche l'appareil photo 
+  Serial.println( "Recule et capture terminé" );
+  if (intRetourAuto == 1)                      // retour au point de départ
+  {
+    digitalWrite( pinDir8825   , HIGH);        // inverse la direction
+    digitalWrite( pinEnable8825, LOW );        // Active le moteur
+    for ( i = 0; i < intPasAccompli; i++) {
+      digitalWrite( pinStep8825, HIGH );
+      delayMicroseconds(timePulseStep);
+      digitalWrite( pinStep8825, LOW );
+      delayMicroseconds(timePulseStep);
+      test_FinAVNC();
+       if (digitalRead(PinIntFinAVNC) == LOW) {
+        i = intPasAccompli;
+      }
+    }
+  }
+  if (intStopFinBoucle == 1)                    // STOP à la fin
+  {
+    digitalWrite( pinEnable8825, HIGH );        // Désactive le moteur   
+  }
+}
+
---
+++ Pilote_Rail_Macro_et_Declencheur_00/do_Commandes.h
@@ -0,0 +1,93 @@
+// ------------------------------------------------------------------------------- 
+//                 Exécution des commandes reçues
+// ------------------------------------------------------------------------------- 
+
+void do_Commandes(){
+          
+         if (currentLine.startsWith("GET /action_page.php?")){
+         // Trouver la position du début de la donnée de chaque variable et lire cette donnée
+         int intPosResolutionDuRail = currentLine.indexOf("ResolutionDuRail=");
+         int intPosDelaiAvantDeclenchement = currentLine.indexOf("DelaiAvantDeclenchement=");
+         int intPosDelaiApresDeclenchement = currentLine.indexOf("DelaiApresDeclenchement=");
+         int intPosDistanceEntreCaptureVR = currentLine.indexOf("DistanceEntreCaptureVR=");
+         int intPosNbreDePasEntreArrets = currentLine.indexOf("NbreDePasEntreArrets=");
+         int intPosNbreDeBoucle = currentLine.indexOf("NbreDeBoucle=");
+         int intPosDistanceEntreCaptureVD = currentLine.indexOf("DistanceEntreCaptureVD=");
+         int intPosDeplacementTotal = currentLine.indexOf("DeplacementTotal=");
+         int intPosHTTP = currentLine.indexOf("HTTP");
+           if (intPosResolutionDuRail >0) { 
+             Serial.println("");
+             Serial.println("***" );
+             Serial.print(intPosResolutionDuRail);
+             Serial.println(" position de ResolutionDuRail" );
+             Serial.print(intPosDelaiAvantDeclenchement);
+             Serial.println(" position de DelaiAvantDeclenchement" );
+             Serial.print(intPosDelaiApresDeclenchement);
+             Serial.println(" position de DelaiApresDeclenchement" );
+             Serial.print(intPosDistanceEntreCaptureVR);
+             Serial.println(" position de DistanceEntreCaptureVR" );
+             Serial.print(intPosNbreDePasEntreArrets);
+             Serial.println(" position de NbreDePasEntreArrets" );
+             Serial.print(intPosNbreDeBoucle);
+             Serial.println(" position de NbreDeBoucle" );
+             Serial.print(intPosDistanceEntreCaptureVD);
+             Serial.println(" position de DistanceEntreCaptureVD" );
+             Serial.print(intPosDeplacementTotal);
+             Serial.println(" position de DeplacementTotal" );
+             //Serial.println(currentLine);
+
+             strDistParPas = currentLine.substring(intPosResolutionDuRail+17,intPosDelaiAvantDeclenchement-1);
+             strDelaiAvantDeclenchement = currentLine.substring(intPosDelaiAvantDeclenchement+24,intPosDelaiApresDeclenchement-1);
+             strDelaiApresDeclenchement = currentLine.substring(intPosDelaiApresDeclenchement+24,intPosDistanceEntreCaptureVR-1);
+             strDistanceEntreCaptureVR = currentLine.substring(intPosDistanceEntreCaptureVR+23,intPosNbreDePasEntreArrets-1);
+             strNbreDePasEntreArrets = currentLine.substring(intPosNbreDePasEntreArrets+21,intPosNbreDeBoucle-1);
+             strNbreDeBoucle = currentLine.substring(intPosNbreDeBoucle+13,intPosDistanceEntreCaptureVD-1);
+             strDistanceEntreCaptureVD = currentLine.substring(intPosDistanceEntreCaptureVD+23,intPosDeplacementTotal-1);
+             strDeplacementTotal = currentLine.substring(intPosDeplacementTotal+17,intPosHTTP-1);
+                
+             do_Conversion_calcul();
+             bolSetParametreRepere = false;
+           }                // fin du if (intPosResolutionDuRail >0) { 
+        }                   // fin du if (currentLine.startsWith("GET /action_page.php?")){
+           
+        if (currentLine.endsWith("GET /PhotoClic HTTP/1.1")) {
+           boExecDeclencheApp = true; 
+        }
+        
+        if (currentLine.endsWith("GET /AvanceLente HTTP/1.1")) {
+           boExecAvanceLente = true;  
+        }    
+        if (currentLine.endsWith("GET /AvanceRapide HTTP/1.1")) {
+           boExecAvanceRapide = true; 
+        } 
+        if (currentLine.endsWith("GET /AvanceTresRapide HTTP/1.1")) {
+           boExecAvanceTresRapide = true; 
+        }       
+        if (currentLine.endsWith("GET /STOP HTTP/1.1")) {
+           boExecStop = true;;  
+        }
+        if (currentLine.endsWith("GET /ReculeLente HTTP/1.1")) {
+           boExecReculeLente = true;  
+        }    
+        if (currentLine.endsWith("GET /ReculeRapide HTTP/1.1")) {
+           boExecReculeRapide = true;
+        } 
+        if (currentLine.endsWith("GET /ReculeTresRapide HTTP/1.1")) {
+           boExecReculeTresRapide = true;  
+        } 
+        if (currentLine.endsWith("GET /RepereA HTTP/1.1")) {
+           boExecRepereA = true;    
+        } 
+        if (currentLine.endsWith("GET /RepereB HTTP/1.1")) {
+           boExecRepereB = true; 
+           bolSetParametreRepere = true;
+        } 
+        if (currentLine.endsWith("GET /ACapture HTTP/1.1")) {
+           boExecAvanceCapture = true;    
+        }
+        if (currentLine.endsWith("GET /RCapture HTTP/1.1")) {
+           boExecReculeCapture = true;   
+        }
+
+}
+
