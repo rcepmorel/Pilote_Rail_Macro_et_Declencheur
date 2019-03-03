c65d07b728b24d4f32484329cf686ccb406800b6
rcepmorel <47545037+rcepmorel@users.noreply.github.com>
2019-03-03 03:30:58 -0500

    Update connect_id_Wifi.h

--- Pilote_Rail_Macro_et_Declencheur_00/connect_id_Wifi.h
+++ Pilote_Rail_Macro_et_Declencheur_00/connect_id_Wifi.h
@@ -1,13 +1,13 @@
 const char* ssid     = "Votre nom de réseau WiFi"; // Réseau à accéder
 const char* password = "Mot de passe du réseau WiFi";
 
-const char* ssid_AP = "La_Porte_Du_Web_03"; // Point d'accès 
-const char* password_AP = "thereisnospoon";
+const char* ssid_AP = "PointDAccesRail"; // Point d'accès 
+const char* password_AP = "aVotreGuise";
 
 
-// Set your Static IP address
+// Configuration adresse IP statique
 IPAddress local_IP(192, 168, 1, 141);
-// Set your Gateway IP address
+// Adresse IP de votre Passerelle
 IPAddress gateway(192, 168, 1, 1);
 
 IPAddress subnet(255, 255, 0, 0);
